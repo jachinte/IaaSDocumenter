b335912393ce1b528da318e9cf774a6e1a3b2374
Raymond Edah <raymond.edah@kellogg.ox.ac.uk>
2015-06-13 10:17:19 +0100

    added debug stuff to main

--- src/main/java/com/eu/skyblue/iaasdocumenter/Main.java
+++ src/main/java/com/eu/skyblue/iaasdocumenter/Main.java
@@ -1,5 +1,16 @@
 package com.eu.skyblue.iaasdocumenter;
 
+import com.amazonaws.regions.Regions;
+import com.eu.skyblue.iaasdocumenter.documenter.IaasDocumenter;
+import com.eu.skyblue.iaasdocumenter.documenter.aws.DocumenterFactory;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.ui.layout.HierarchicalLayout;
+import org.graphstream.ui.view.Viewer;
+
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: raye
@@ -8,7 +19,31 @@ package com.eu.skyblue.iaasdocumenter;
  * To change this template use File | Settings | File Templates.
  */
 public class Main {
+    private static IaasDocumenter iaasDocumenter;
     public static void main(String[] args) {
-        System.out.println("IaaSDocumenter!");
+        System.out.println("*** IaaSDocumenter! ***");
+
+        try {
+            iaasDocumenter = DocumenterFactory.createIaasDocumenter(Regions.US_WEST_2);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        iaasDocumenter.createInventory();
+
+        // Debug stuff - should be moved into a renderer
+        List<Graph> g = iaasDocumenter.getGraphs();
+        for (Graph graph : iaasDocumenter.getGraphs()) {
+            Viewer v = graph.display(false);
+            HierarchicalLayout hl = new HierarchicalLayout();
+            v.enableAutoLayout(hl);
+
+            Iterator<Node> nodeIterator = graph.iterator();
+            while (nodeIterator.hasNext()) {
+                System.out.println(nodeIterator.next().getId());
+            }
+            System.out.println("--------------------------------------------");
+        }
+
     }
 }
