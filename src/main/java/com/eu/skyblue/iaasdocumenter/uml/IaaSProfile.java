b8f72d904e1a3d282249dca400f18b5c87d17f92
Raymond Edah <raymond.edah@kellogg.ox.ac.uk>
2015-06-19 08:20:28 +0100

    Removed applyNodeStereotype, added applyStereotype. Added accesors for defined stereotypes.

--- src/main/java/com/eu/skyblue/iaasdocumenter/uml/IaaSProfile.java
+++ src/main/java/com/eu/skyblue/iaasdocumenter/uml/IaaSProfile.java
@@ -96,113 +96,113 @@ public class IaaSProfile {
 
     private void createStereotypeRouter() {
         this.routerStereotype = umlProfileBuilder.createStereotype(UMLStereotype.ROUTER, Boolean.FALSE);
-        Property vpcCidr = umlProfileBuilder.createAttribute(this.routerStereotype, UMLStereotypeProperty.VPC_CIDR,
+        Property vpcCidr = umlProfileBuilder.createAttribute(this.getRouterStereotype(), UMLStereotypeProperty.VPC_CIDR,
                 stringPrimitiveType, 0, 1, null);
-        umlProfileBuilder.createExtension(this.nodeMetaclass, this.routerStereotype, Boolean.FALSE);
+        umlProfileBuilder.createExtension(this.nodeMetaclass, this.getRouterStereotype(), Boolean.FALSE);
     }
 
     private void createStereotypeAWSNode() {
         this.awsNodeStereotype = umlProfileBuilder.createStereotype(UMLStereotype.AWS_NODE, Boolean.TRUE);
-        Property id = umlProfileBuilder.createAttribute(this.awsNodeStereotype, UMLStereotypeProperty.ID,
+        Property id = umlProfileBuilder.createAttribute(this.getAwsNodeStereotype(), UMLStereotypeProperty.ID,
                 stringPrimitiveType, 0, 1, null);
-        umlProfileBuilder.createExtension(this.nodeMetaclass, this.awsNodeStereotype, Boolean.FALSE);
+        umlProfileBuilder.createExtension(this.nodeMetaclass, this.getAwsNodeStereotype(), Boolean.FALSE);
     }
 
     private void createStereotypeFirewall() {
         this.firewallStereotype = umlProfileBuilder.createStereotype(UMLStereotype.FIREWALL, Boolean.TRUE);
-        Property name = umlProfileBuilder.createAttribute(this.firewallStereotype, UMLStereotypeProperty.NAME,
+        Property name = umlProfileBuilder.createAttribute(this.getFirewallStereotype(), UMLStereotypeProperty.NAME,
                 stringPrimitiveType, 0, 1, null);
-        umlProfileBuilder.createExtension(this.nodeMetaclass, this.firewallStereotype, Boolean.FALSE);
-        Generalization generalization = umlProfileBuilder.createGeneralization(this.firewallStereotype,
-                this.awsNodeStereotype);
+        umlProfileBuilder.createExtension(this.nodeMetaclass, this.getFirewallStereotype(), Boolean.FALSE);
+        Generalization generalization = umlProfileBuilder.createGeneralization(this.getFirewallStereotype(),
+                this.getAwsNodeStereotype());
     }
 
     private void createStereotypeElasticLB() {
         this.elasticLBStereotype = umlProfileBuilder.createStereotype(UMLStereotype.ELASTIC_LB, Boolean.FALSE);
-        Property name = umlProfileBuilder.createAttribute(this.elasticLBStereotype, UMLStereotypeProperty.NAME,
+        Property name = umlProfileBuilder.createAttribute(this.getElasticLBStereotype(), UMLStereotypeProperty.NAME,
                 stringPrimitiveType, 0, 1, null);
-        Property dnsName = umlProfileBuilder.createAttribute(this.elasticLBStereotype, UMLStereotypeProperty.DNS_NAME,
+        Property dnsName = umlProfileBuilder.createAttribute(this.getElasticLBStereotype(), UMLStereotypeProperty.DNS_NAME,
                 stringPrimitiveType, 0, 1, null);
-        umlProfileBuilder.createExtension(this.nodeMetaclass, this.elasticLBStereotype, Boolean.FALSE);
-        Generalization generalization = umlProfileBuilder.createGeneralization(this.elasticLBStereotype,
-                this.awsNodeStereotype);
+        umlProfileBuilder.createExtension(this.nodeMetaclass, this.getElasticLBStereotype(), Boolean.FALSE);
+        Generalization generalization = umlProfileBuilder.createGeneralization(this.getElasticLBStereotype(),
+                this.getAwsNodeStereotype());
     }
 
     private void createStereotypeInternetGateway() {
         this.internetGatewayStereotype = umlProfileBuilder.createStereotype(UMLStereotype.INTERNET_GATEWAY,
                 Boolean.FALSE);
-        Property name = umlProfileBuilder.createAttribute(this.internetGatewayStereotype, UMLStereotypeProperty.NAME,
+        Property name = umlProfileBuilder.createAttribute(this.getInternetGatewayStereotype(), UMLStereotypeProperty.NAME,
                 stringPrimitiveType, 0, 1, null);
-        umlProfileBuilder.createExtension(this.nodeMetaclass, this.internetGatewayStereotype, Boolean.FALSE);
-        Generalization generalization = umlProfileBuilder.createGeneralization(this.internetGatewayStereotype,
-                this.awsNodeStereotype);
+        umlProfileBuilder.createExtension(this.nodeMetaclass, this.getInternetGatewayStereotype(), Boolean.FALSE);
+        Generalization generalization = umlProfileBuilder.createGeneralization(this.getInternetGatewayStereotype(),
+                this.getAwsNodeStereotype());
     }
 
     private void createStereotypeEC2Instance() {
         this.ec2InstanceStereotype = umlProfileBuilder.createStereotype(UMLStereotype.EC2_INSTANCE, Boolean.FALSE);
-        Property ami = umlProfileBuilder.createAttribute(this.ec2InstanceStereotype, UMLStereotypeProperty.AMI,
+        Property ami = umlProfileBuilder.createAttribute(this.getEc2InstanceStereotype(), UMLStereotypeProperty.AMI,
                 stringPrimitiveType, 0, 1, null);
-        umlProfileBuilder.createExtension(this.nodeMetaclass, this.ec2InstanceStereotype, Boolean.FALSE);
-        Generalization generalization = umlProfileBuilder.createGeneralization(this.ec2InstanceStereotype,
-                this.awsNodeStereotype);
+        umlProfileBuilder.createExtension(this.nodeMetaclass, this.getEc2InstanceStereotype(), Boolean.FALSE);
+        Generalization generalization = umlProfileBuilder.createGeneralization(this.getEc2InstanceStereotype(),
+                this.getAwsNodeStereotype());
     }
 
     private void createStereotypeSecurityGroup() {
         this.securityGroupStereotype = umlProfileBuilder.createStereotype(UMLStereotype.SECURITY_GROUP, Boolean.FALSE);
-        umlProfileBuilder.createExtension(this.nodeMetaclass, this.securityGroupStereotype, Boolean.FALSE);
-        Generalization generalization = umlProfileBuilder.createGeneralization(this.securityGroupStereotype,
-                this.firewallStereotype);
+        umlProfileBuilder.createExtension(this.nodeMetaclass, this.getSecurityGroupStereotype(), Boolean.FALSE);
+        Generalization generalization = umlProfileBuilder.createGeneralization(this.getSecurityGroupStereotype(),
+                this.getFirewallStereotype());
     }
 
     private void createStereotypeNetworkACL() {
         this.networkAclStereotype = umlProfileBuilder.createStereotype(UMLStereotype.NETWORK_ACL, Boolean.FALSE);
-        umlProfileBuilder.createExtension(this.nodeMetaclass, this.networkAclStereotype, Boolean.FALSE);
-        Generalization generalization = umlProfileBuilder.createGeneralization(this.networkAclStereotype,
-                this.firewallStereotype);
+        umlProfileBuilder.createExtension(this.nodeMetaclass, this.getNetworkAclStereotype(), Boolean.FALSE);
+        Generalization generalization = umlProfileBuilder.createGeneralization(this.getNetworkAclStereotype(),
+                this.getFirewallStereotype());
     }
 
     private void createStereotypeVirtualPrivateCloud() {
         this.virtualPrivateCloudStereotype = umlProfileBuilder.createStereotype(UMLStereotype.VPC, Boolean.FALSE);
-        Property id = umlProfileBuilder.createAttribute(this.virtualPrivateCloudStereotype, UMLStereotypeProperty.ID,
+        Property id = umlProfileBuilder.createAttribute(this.getVirtualPrivateCloudStereotype(), UMLStereotypeProperty.ID,
                 stringPrimitiveType, 0, 1, null);
-        Property isDefault = umlProfileBuilder.createAttribute(this.virtualPrivateCloudStereotype,
-                UMLStereotypeProperty.IS_DEFAULT, stringPrimitiveType, 0, 1, null);
-        Property cidrBlock = umlProfileBuilder.createAttribute(this.virtualPrivateCloudStereotype,
+        Property isDefault = umlProfileBuilder.createAttribute(this.getVirtualPrivateCloudStereotype(),
+                UMLStereotypeProperty.IS_DEFAULT, booleanPrimitiveType, 0, 1, null);
+        Property cidrBlock = umlProfileBuilder.createAttribute(this.getVirtualPrivateCloudStereotype(),
                 UMLStereotypeProperty.CIDR_BLOCK, stringPrimitiveType, 0, 1, null);
-        umlProfileBuilder.createExtension(this.artefactMetaclass, this.virtualPrivateCloudStereotype, Boolean.FALSE);
+        umlProfileBuilder.createExtension(this.artefactMetaclass, this.getVirtualPrivateCloudStereotype(), Boolean.FALSE);
     }
 
     private void createStereotypeSubnet() {
         this.subnetStereotype = umlProfileBuilder.createStereotype(UMLStereotype.SUBNET, Boolean.FALSE);
-        Property isDefault = umlProfileBuilder.createAttribute(this.subnetStereotype,
-                UMLStereotypeProperty.IS_DEFAULT, stringPrimitiveType, 0, 1, null);
-        Property cidrBlock = umlProfileBuilder.createAttribute(this.subnetStereotype,
+        Property isDefault = umlProfileBuilder.createAttribute(this.getSubnetStereotype(),
+                UMLStereotypeProperty.IS_DEFAULT, booleanPrimitiveType, 0, 1, null);
+        Property cidrBlock = umlProfileBuilder.createAttribute(this.getSubnetStereotype(),
                 UMLStereotypeProperty.CIDR_BLOCK, stringPrimitiveType, 0, 1, null);
-        Property availabilityZone = umlProfileBuilder.createAttribute(this.subnetStereotype,
+        Property availabilityZone = umlProfileBuilder.createAttribute(this.getSubnetStereotype(),
                 UMLStereotypeProperty.AVAILABILITY_ZONE, stringPrimitiveType, 0, 1, null);
-        umlProfileBuilder.createExtension(this.artefactMetaclass, this.subnetStereotype, Boolean.FALSE);
+        umlProfileBuilder.createExtension(this.artefactMetaclass, this.getSubnetStereotype(), Boolean.FALSE);
     }
 
     private void createStereotypeRouteTable() {
         this.routeTableStereotype = umlProfileBuilder.createStereotype(UMLStereotype.ROUTE_TABLE, Boolean.FALSE);
-        umlProfileBuilder.createExtension(this.artefactMetaclass, this.routeTableStereotype, Boolean.FALSE);
+        umlProfileBuilder.createExtension(this.artefactMetaclass, this.getRouteTableStereotype(), Boolean.FALSE);
     }
 
     private void createStereotypeNetworkInterface() {
         this.networkInterfaceStereotype = umlProfileBuilder.createStereotype(UMLStereotype.ENI, Boolean.FALSE);
-        umlProfileBuilder.createExtension(this.artefactMetaclass, this.networkInterfaceStereotype, Boolean.FALSE);
+        umlProfileBuilder.createExtension(this.artefactMetaclass, this.getNetworkInterfaceStereotype(), Boolean.FALSE);
     }
 
     private void createStereotypePacketFiltering() {
         this.packetFilteringStereotype = umlProfileBuilder.createStereotype(UMLStereotype.PACKET_FILTERING,
                 Boolean.FALSE);
-        umlProfileBuilder.createExtension(this.associationMetaclass, this.packetFilteringStereotype, Boolean.FALSE);
+        umlProfileBuilder.createExtension(this.associationMetaclass, this.getPacketFilteringStereotype(), Boolean.FALSE);
     }
 
     private void createStereotypeOSILayer2Link() {
         this.osiLayer2LinkStereotype = umlProfileBuilder.createStereotype(UMLStereotype.OSI_LAYER2_LINK,
                 Boolean.FALSE);
-        umlProfileBuilder.createExtension(this.associationMetaclass, this.osiLayer2LinkStereotype, Boolean.FALSE);
+        umlProfileBuilder.createExtension(this.associationMetaclass, this.getOsiLayer2LinkStereotype(), Boolean.FALSE);
     }
 
     public void applyProfile(org.eclipse.uml2.uml.Package model) {
@@ -212,58 +212,10 @@ public class IaaSProfile {
                 model.getQualifiedName());
     }
 
-    // code smell?
-    public void applyNodeStereotype(NamedElement namedElement, String stereotype) {
-        Stereotype stereotypeToApply;
-        switch (stereotype) {
-            case UMLStereotype.ROUTER:
-                stereotypeToApply = routerStereotype;
-                break;
-            case UMLStereotype.AWS_NODE:
-                stereotypeToApply = awsNodeStereotype;
-                break;
-            case UMLStereotype.FIREWALL:
-                stereotypeToApply = firewallStereotype;
-                break;
-            case UMLStereotype.ELASTIC_LB:
-                stereotypeToApply = elasticLBStereotype;
-                break;
-            case UMLStereotype.INTERNET_GATEWAY:
-                stereotypeToApply = internetGatewayStereotype;
-                break;
-            case UMLStereotype.EC2_INSTANCE:
-                stereotypeToApply = ec2InstanceStereotype;
-                break;
-            case UMLStereotype.SECURITY_GROUP:
-                stereotypeToApply = securityGroupStereotype;
-                break;
-            case UMLStereotype.NETWORK_ACL:
-                stereotypeToApply = networkAclStereotype;
-                break;
-            case UMLStereotype.VPC:
-                stereotypeToApply = virtualPrivateCloudStereotype;
-                break;
-            case UMLStereotype.SUBNET:
-                stereotypeToApply = subnetStereotype;
-                break;
-            case UMLStereotype.ROUTE_TABLE:
-                stereotypeToApply = routeTableStereotype;
-                break;
-            case UMLStereotype.ENI:
-                stereotypeToApply = networkInterfaceStereotype;
-                break;
-            case UMLStereotype.PACKET_FILTERING:
-                stereotypeToApply = packetFilteringStereotype;
-                break;
-            case UMLStereotype.OSI_LAYER2_LINK:
-                stereotypeToApply = osiLayer2LinkStereotype;
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid stereotype");
-        }
-        namedElement.applyStereotype(stereotypeToApply);
+    public void applyStereotype(NamedElement namedElement, Stereotype stereotype) {
+        namedElement.applyStereotype(stereotype);
 
-        logger.out("Stereotype '%s' applied to element '%s'.", routerStereotype.getQualifiedName(),
+        logger.out("Stereotype '%s' applied to element '%s'.", stereotype.getQualifiedName(),
                 namedElement.getQualifiedName());
     }
 
@@ -283,5 +235,59 @@ public class IaaSProfile {
                 namedElement.getQualifiedName(), value);
     }
 
+    public Stereotype getRouterStereotype() {
+        return routerStereotype;
+    }
 
+    public Stereotype getAwsNodeStereotype() {
+        return awsNodeStereotype;
+    }
+
+    public Stereotype getFirewallStereotype() {
+        return firewallStereotype;
+    }
+
+    public Stereotype getElasticLBStereotype() {
+        return elasticLBStereotype;
+    }
+
+    public Stereotype getInternetGatewayStereotype() {
+        return internetGatewayStereotype;
+    }
+
+    public Stereotype getEc2InstanceStereotype() {
+        return ec2InstanceStereotype;
+    }
+
+    public Stereotype getSecurityGroupStereotype() {
+        return securityGroupStereotype;
+    }
+
+    public Stereotype getNetworkAclStereotype() {
+        return networkAclStereotype;
+    }
+
+    public Stereotype getVirtualPrivateCloudStereotype() {
+        return virtualPrivateCloudStereotype;
+    }
+
+    public Stereotype getSubnetStereotype() {
+        return subnetStereotype;
+    }
+
+    public Stereotype getRouteTableStereotype() {
+        return routeTableStereotype;
+    }
+
+    public Stereotype getNetworkInterfaceStereotype() {
+        return networkInterfaceStereotype;
+    }
+
+    public Stereotype getPacketFilteringStereotype() {
+        return packetFilteringStereotype;
+    }
+
+    public Stereotype getOsiLayer2LinkStereotype() {
+        return osiLayer2LinkStereotype;
+    }
 }
